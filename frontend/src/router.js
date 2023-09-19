e568058c37b9c062885e692400a6106071a72b24
AntoineQuarroz <antoine.quarroz@hes-so.ch>
2023-09-19 14:23:29 +0200

    ajout de la page error404

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from "./components/HomePage.vue";
 import Map from "./components/Map.vue";
+import Error404 from "./components/Error404.vue";
 
 const routes = [
     { path: '/', component: HomePage },
     { path: '/map', component: Map },
+    { path: '/:pathMatch(.*)*', component: Error404 }
 
     // ... autres routes si nécessaires
 ]
